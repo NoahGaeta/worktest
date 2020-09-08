5ec94257e92c779e6054af1271ee5ca41e0913e9
Noah Gaeta <ngaet02@gmail.com>
2020-09-08 03:02:35 -0400

    Added challenge 1 solution

--- challenge-1/steering_angle.py
+++ challenge-1/steering_angle.py
@@ -6,10 +6,19 @@ Author: Noah Gaeta
 import math
 from sys import argv
 
+A = 5
+B = math.sqrt(113)  # found manually using distance formula
+INIT_THETA = math.atan(7 / 8)  # initial theta value at '0'
+
+
 def main():
-    x_dist = float(argv[1])
-    theta = math.acos(3 / (5 + x_dist))
-    print("Steering angle is: ", theta)
+    x = float(argv[1])
+    c = x + 5
+    numerator = A**2 + B**2  - c**2 # (a^2 + b^2 - c^2)
+    denominator = 2 * A * B  # 2ab
+    theta = math.acos((numerator / denominator))  # find theta using law of cosines
+    theta = INIT_THETA - theta  # subtract relative theta from initial theta
+    print("Steering angle is: %f radians" % theta)
     return 0
 
 if __name__ == '__main__':
